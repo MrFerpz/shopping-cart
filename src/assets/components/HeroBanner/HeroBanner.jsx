f248c67363848cf7bcc1d04baf7c106bc5e0b772
Fred <leferpz@gmail.com>
2025-02-11 17:29:57 -0500

    Hero banner in place

--- src/assets/components/HeroBanner/HeroBanner.jsx
+++ src/assets/components/HeroBanner/HeroBanner.jsx
@@ -1,12 +1,16 @@
 import styles from "./HeroBanner.module.css"
 import yogPic1 from "./yog1.png";
+import { Link } from "react-router-dom";
 
 function HeroBanner() {
     return (
         <section className={styles.heroBannerContainer}>
-            <h1>Yoghurt directly to your doorstep</h1>
-            <h2>Get your mitts on these yogs while they last</h2>
-            <img src={yogPic1} alt="Yoghurt with strawberries" width="300"></img>
+            <div className={styles.verticalContainer}>
+                <h1>Yoghurt directly to your doorstep...</h1>
+                <h2>Get your mitts on these yogs ASAP!</h2>
+                <button><Link to="shop">Shop Yog</Link></button>
+            </div>
+            <img src={yogPic1} alt="Yoghurt with strawberries" className={styles.yoghurtPic}></img>
         </section>
     )
 }
